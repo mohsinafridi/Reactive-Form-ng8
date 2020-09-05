84935f76f03224fb96bff8b78e33fc8088077305
moaz <mohsin.afridi91@gmail.com>
2020-09-05 13:02:40 +0500

    pagenotfound added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { PageNotFoundComponent } from './page-not-found.component';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
